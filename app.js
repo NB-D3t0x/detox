1dd8a0be534d3496ab0f6cf35c78b8cc6dc682ac
NB-D3t0x <albertonarvaez@gmail.com>
2022-03-19 18:21:10 -0600

    probando append
    Signed-off-by: NB-D3t0x <albertonarvaez@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 /*
 
-Tipos de comentarios
-para distintas formas 
-de poner multi linea
-para bloque
+Tipos de comentarios para distintas formas 
+de poner multi linea para bloque
 
 */
 
@@ -12,3 +10,20 @@ para bloque
 var hola = "saludos"
 console.log(hola)
 console.log("adios")
+
+/*
+Tipos de datos
+*/
+
+//NUMBER para to tipos de numeros enteros y decimales
+console.log(typeof 100)
+
+//string o cadena de texto "" o ''
+
+console.log(typeof "Alberto Narvaez")
+
+// boolean verdadero o falso
+
+let profesor = "Alberto Narvaez Boersner"
+
+$('div').append("<p>" + profesor + "</p>")
