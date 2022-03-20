3c6bc23a8365b6fc0e896ea301f708754098ef80
NB-D3t0x <albertonarvaez@gmail.com>
2022-03-19 21:10:31 -0600

    api
    Signed-off-by: NB-D3t0x <albertonarvaez@gmail.com>

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-let profesor = "Alberto Narvaez Boersner"
-$('div').append("<p>" + profesor + "</p>")
+$.getJSON( "https://gorest.co.in/public/v2/posts", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + '<h1>' + val.title + '</h1>' + '<p>' +  val.body + '</p> '+"</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
