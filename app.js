5aefe178cf620ef5dda1d003954b7fdd1222169a
NB-D3t0x <albertonarvaez@gmail.com>
2022-03-19 18:49:30 -0600

    app
    Signed-off-by: NB-D3t0x <albertonarvaez@gmail.com>

--- app.js
+++ app.js
@@ -1,29 +1,2 @@
-/*
-
-Tipos de comentarios para distintas formas 
-de poner multi linea para bloque
-
-*/
-
-// una sola linea de comentario
-
-var hola = "saludos"
-console.log(hola)
-console.log("adios")
-
-/*
-Tipos de datos
-*/
-
-//NUMBER para to tipos de numeros enteros y decimales
-console.log(typeof 100)
-
-//string o cadena de texto "" o ''
-
-console.log(typeof "Alberto Narvaez")
-
-// boolean verdadero o falso
-
 let profesor = "Alberto Narvaez Boersner"
-
 $('div').append("<p>" + profesor + "</p>")
