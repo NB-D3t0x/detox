9a59f3d41bc7f151498299d5e3548afab36411ef
NB-D3t0x <albertonarvaez@gmail.com>
2022-03-19 23:36:35 -0600

    mit
    Signed-off-by: NB-D3t0x <albertonarvaez@gmail.com>

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 $.getJSON( "https://gorest.co.in/public/v2/posts", function( data ) {
-  var items = [];
+  var items = []
   $.each( data, function( key, val ) {
     //aqui puedes ingresar todos los valores que viene por defecto por el objeto del api
-    items.push( "<div id='" + key + "' class='cards'>" + '<h1>' + val.title + '</h1>' + '<p>' +  val.body + '</p> '+"</div>" );
-  });
+    items.push( "<div id='" + key + "' class='cards'>" + '<h1>' + val.title + '</h1>' + '<p>' +  val.body + '</p> '+"</div>" )
+  })
   $( "<div/>", {
     "class": "my-new-list",
     html: items.join( "" )
-  }).appendTo( "body" );
-});
+  }).appendTo( "body" )
+})
